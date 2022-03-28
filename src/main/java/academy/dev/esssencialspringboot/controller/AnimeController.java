2a54bd6676d37ca1729903eeaf47dcb129e4acda
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-03-28 17:05:25 -0300

    fix: make route to find by id

--- src/main/java/academy/dev/esssencialspringboot/controller/AnimeController.java
+++ src/main/java/academy/dev/esssencialspringboot/controller/AnimeController.java
@@ -5,7 +5,10 @@ import academy.dev.esssencialspringboot.service.AnimeService;
 import academy.dev.esssencialspringboot.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +25,13 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDatetimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/academy/dev/esssencialspringboot/service/AnimeService.java
+++ src/main/java/academy/dev/esssencialspringboot/service/AnimeService.java
@@ -1,13 +1,26 @@
 package academy.dev.esssencialspringboot.service;
 
 import academy.dev.esssencialspringboot.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+
+    private List<Anime> animes = List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Naruto"));
+
     public List<Anime> listAll() {
-        return List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Naruto"));
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes
+                .stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
