ed318df5cb44b92a10fce24f3162aa7ad22bf45a
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-03-28 22:04:19 -0300

    fix: create route to edit, delete e find by id

--- src/main/java/academy/dev/esssencialspringboot/service/AnimeService.java
+++ src/main/java/academy/dev/esssencialspringboot/service/AnimeService.java
@@ -5,12 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Naruto"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Naruto")));
+    }
 
     public List<Anime> listAll() {
         return animes;
@@ -23,4 +29,16 @@ public class AnimeService {
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
+
+    public Anime create(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,100000));
+        animes.add(anime);
+
+        return anime;
+    }
+
+    public void delete(long id) {
+        animes.remove(findById(id));
+    }
+
 }
